4853d206c42721d49ad95ed14badb7481a287850
codergo7 <codergo7@gmail.com>
2020-10-06 15:20:30 +0200

    added new project of for loop

--- Looping/src/be/intecbrussel/excercises/Excercise17_06.java
+++ Looping/src/be/intecbrussel/excercises/Excercise17_06.java
@@ -3,5 +3,13 @@ package be.intecbrussel.excercises;
 public class Excercise17_06 {
     public static void main(String[] args) {
 
+        String str;
+        for(int i = -10; i<=10;i++){
+            if(i>0){
+                System.out.println("+" + i);
+            }
+            else System.out.println(i);
+        }
+
     }
 }
