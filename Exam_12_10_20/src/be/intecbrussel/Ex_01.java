31565d079acace5998df5bf10d2acaca7eed8f74
codergo7 <codergo7@gmail.com>
2020-10-12 10:45:00 +0200

    added solution of ex1

---
+++ Exam_12_10_20/src/be/intecbrussel/Ex_01.java
@@ -0,0 +1,12 @@
+package be.intecbrussel;
+
+public class Ex_01 {
+    public static void main(String[] args) {
+
+        for(int number = 1; number<=99; number++ ){
+            if(number%2==0){
+                System.out.println(number);
+            }
+        }
+    }
+}
