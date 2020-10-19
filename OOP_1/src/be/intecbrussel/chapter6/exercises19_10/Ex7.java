2fc8093bb795d41927cac5fc7e0c6f92a3e035bd
codergo7 <codergo7@gmail.com>
2020-10-19 11:52:25 +0200

    added solution of ex7

---
+++ OOP_1/src/be/intecbrussel/chapter6/exercises19_10/Ex7.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.chapter6.exercises19_10;
+
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a text please ");
+        String text = scanner.nextLine();
+
+        System.out.println("There are " + amountOfSpace(text) + " spaces");
+    }
+    public static int amountOfSpace(String text){
+        int counter =0;
+        for (int i = 0; i <text.length() ; i++) {
+            if(text.charAt(i)== ' '){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
