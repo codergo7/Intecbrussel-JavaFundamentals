588b37a76a5ced63806e6d6ab03fe69f0bc04f7e
codergo7 <codergo7@gmail.com>
2020-10-19 11:36:37 +0200

    added solution of ex4

---
+++ OOP_1/src/be/intecbrussel/chapter6/examen19_10/Ex4.java
@@ -0,0 +1,26 @@
+package be.intecbrussel.chapter6.examen19_10;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex4 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int step =0;
+
+        int rand = random.nextInt(10);
+        //System.out.println(rand);
+        System.out.println("Guess a number between 1 and 10 please");
+        int guess = scanner.nextInt();
+        while (rand != guess){
+            System.out.println("Wrong guess!");
+            System.out.println("Guess again: ");
+            guess = scanner.nextInt(10);
+            step++;
+        }
+        System.out.println("Congratulations you guess ture " + step + " steps");
+
+    }
+}
