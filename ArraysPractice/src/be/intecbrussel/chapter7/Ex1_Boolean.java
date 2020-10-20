5a1b1027c7c577b93171ba698df9456ebd3eb9e1
codergo7 <codergo7@gmail.com>
2020-10-20 12:34:37 +0200

    added Ex1_3

---
+++ ArraysPractice/src/be/intecbrussel/chapter7/Ex1_Boolean.java
@@ -0,0 +1,11 @@
+package be.intecbrussel.chapter7;
+
+public class Ex1_Boolean {
+    public static void main(String[] args) {
+        boolean[] arrBool = new boolean[10];
+
+        for(boolean value:arrBool){
+            System.out.print(value + " ");
+        }
+    }
+}
