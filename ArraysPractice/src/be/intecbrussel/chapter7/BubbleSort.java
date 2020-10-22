d5ebb17ae64f017bc1b9f9ad57d927f0257f3887
codergo7 <codergo7@gmail.com>
2020-10-22 12:34:15 +0200

    added efficient BubbleSort solution

---
+++ ArraysPractice/src/be/intecbrussel/chapter7/BubbleSort.java
@@ -0,0 +1,52 @@
+package testt;
+
+import java.util.Random;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int limit = 50;
+        int[] numbers = new int[limit];
+        numbers = fillRandomArray(rand, numbers);
+        printArr(numbers);
+        numbers = bubbleSortArray(numbers);
+        System.out.println();
+        printArr(numbers);
+
+
+    }
+
+    public static int[] fillRandomArray(Random rand, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        return arr;
+    }
+
+    public static int[] bubbleSortArray(int[] nums) {
+        int amountOfSwap=0;
+        for (int j = 0; j <nums.length; j++) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    int a = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = a;
+                    amountOfSwap++;
+                }
+            }
+            if(amountOfSwap==0) {
+                System.out.println("here" + j);
+                break;
+            }
+            amountOfSwap=0;
+        }
+        return nums;
+    }
+
+    public static void printArr(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
