007d6fd7db8c49766c4a8f18b1df7db2f6eb34b9
codergo7 <codergo7@gmail.com>
2020-10-31 10:05:26 +0100

    rearranged main method

--- Chapter_8_Classes/src/intecbrussel/car_assignment/RandomCarApp.java
+++ Chapter_8_Classes/src/intecbrussel/car_assignment/RandomCarApp.java
@@ -7,54 +7,48 @@ public class RandomCarApp {
     public static void main(String[] args) {
 
         Car[] cars = new Car[20];
-
-        for (int i = 0; i < cars.length ; i++) {
+        System.out.println("\n************ Generate new cars  ************");
+        for (int i = 0; i < cars.length; i++) {
             cars[i] = randomGenerator();
         }
 
-        for (Car car : cars){
+        for (Car car : cars) {
             car.print();
         }
 
-        for(Car car: cars){
+        System.out.println("\n************ Park the cars  ************");
+
+        for (Car car : cars) {
             car.park();
-        }
-
-        for (Car car : cars){
             car.print();
         }
+
     }
 
     public static Car randomGenerator() {
-
-        String[] colors = {"white","red","blue","green", "grey","purple","orange"};
-
         Random random = new Random();
 
-        return new Car(randomHp(), randomSpeed(), randomColor(), randomLight() );
+        return new Car(randomHp(random), randomSpeed(random), randomColor(random), randomLight(random));
     }
 
-    public static String randomColor() {
-
-        String[] colors = {"white","red","blue","green", "grey","purple","orange"};
-
-        Random random = new Random();
+    public static String randomColor(Random random) {
+        String[] colors = {"white", "red", "blue", "green", "grey", "purple", "orange", "black", "yellow"};
 
         return colors[random.nextInt(colors.length)];
     }
-    public static double randomSpeed() {
-        Random random = new Random();
+
+    public static double randomSpeed(Random random) {
         int limit = 100;
-        return random.nextInt(limit) +1;
+        return random.nextInt(limit) + 1;
     }
-    public static int randomHp() {
-        Random random = new Random();
+
+    public static int randomHp(Random random) {
         int limit = 100;
-        return random.nextInt(limit) +1;
+        return random.nextInt(limit) + 1;
     }
-    public static  boolean randomLight() {
-        Random random = new Random();
-        boolean[] light = {true,false};
+
+    public static boolean randomLight(Random random) {
+        boolean[] light = {true, false};
         return light[random.nextInt(2)];
     }
 
