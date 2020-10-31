cb54f638d396ce0a90f82b38ff190259124c7817
codergo7 <codergo7@gmail.com>
2020-10-31 18:49:33 +0100

    updated

--- Chapter_9_ClassRelationships/src/be/intecbrussel/Main.java
+++ Chapter_9_ClassRelationships/src/be/intecbrussel/Main.java
@@ -3,5 +3,5 @@ package be.intecbrussel;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hallo world");    }
+        System.out.println("hello world");    }
 }
