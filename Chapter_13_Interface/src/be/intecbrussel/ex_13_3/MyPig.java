2fa8accf53349d0dda9f774ca52e328642e9dde8
codergo7 <codergo7@gmail.com>
2020-11-16 16:49:49 +0100

    completed

---
+++ Chapter_13_Interface/src/be/intecbrussel/ex_13_3/MyPig.java
@@ -0,0 +1,9 @@
+package be.intecbrussel.ex_13_3;
+
+public class MyPig implements Pig{
+
+    @Override
+    public void grunt() {
+        System.out.println("The pig is grunting");
+    }
+}
---
+++ Chapter_13_Interface/src/be/intecbrussel/ex_13_3/PigApp.java
@@ -0,0 +1,10 @@
+package be.intecbrussel.ex_13_3;
+
+public class PigApp {
+    public static void main(String[] args) {
+        Pig pig = new MyPig();
+
+        pig.grunt();
+        pig.fly();
+    }
+}
