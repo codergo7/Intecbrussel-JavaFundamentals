4572f6a46cec9268387ed6b935d6268e527f063f
codergo7 <codergo7@gmail.com>
2020-11-19 15:38:39 +0100

    completed

---
+++ Chapter_14_InnerClass/src/be/intecbrussel/ex_2/MusicianApp.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.ex_2;
+
+public class MusicianApp {
+
+    public static void main(String[] args) {
+
+        Musician musician = new Musician();
+        System.out.println(musician.getInstrument().toString());
+
+
+    }
+}
