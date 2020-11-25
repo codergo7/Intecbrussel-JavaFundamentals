3ce477848baccb5627dbe0d41e8f2aabaf4578a7
codergo7 <codergo7@gmail.com>
2020-11-25 16:21:34 +0100

    edited

--- Chapter_15_Exception_Handling/src/be/intecbrussel/ex_1/Division.java
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/ex_1/Division.java
@@ -5,29 +5,19 @@ import java.util.Scanner;
 public class Division {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num =0;
-        int den = 0;
-        int div =0;
 
         try {
             System.out.println("Enter first number: ");
-            num = Integer.parseInt(scanner.next());
+            int num = Integer.parseInt(scanner.next());
             System.out.println("Enter second number: ");
-            den = Integer.parseInt(scanner.next());
-            div = num / den;
+            int den = Integer.parseInt(scanner.next());
+            int div = num / den;
 
             System.out.format("%d/%d=%d", num, den, div);
         }catch (NumberFormatException e){
-            System.out.println("Please enter numbers");
-            main(new String[]{});
-
-        } catch (ArithmeticException a){
-            System.out.println("Dividing value can not be zero.");
-            main(new String[]{});
-
+            System.out.println("Invalid number");
+            //main(new String[]{});
         }
-
-
         scanner.close();
     }
 }
