166d48c745b0b1297bd21e156902acb179a6a167
codergo7 <codergo7@gmail.com>
2020-11-25 17:01:26 +0100

    called getMessage and printStackTrace

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/ex_2/Division.java
@@ -0,0 +1,30 @@
+package be.intecbrussel.ex_2;
+
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter first number: ");
+            int num = Integer.parseInt(scanner.next());
+            System.out.println("Enter second number: ");
+            int den = Integer.parseInt(scanner.next());
+            int div = num / den;
+
+            System.out.format("%d/%d=%d", num, den, div);
+            scanner.close();
+        } catch (NumberFormatException nfe) {
+            System.out.println("Invalid number");
+            System.out.println(nfe.getMessage());
+            nfe.printStackTrace();
+
+            //main(new String[]{});
+
+        } catch (ArithmeticException a) {
+            System.out.println("Dividing value can not be zero.");
+            //main(new String[]{});
+        }
+    }
+}
