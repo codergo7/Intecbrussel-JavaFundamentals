3a9ef4d0bfa26ba11e766bd9df9a01f2d21cb612
codergo7 <codergo7@gmail.com>
2020-11-27 00:44:02 +0100

    added NegativeSizeException class

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/ex_6/NegativeSizeException.java
@@ -0,0 +1,19 @@
+package be.intecbrussel.ex_6;
+
+public class NegativeSizeException extends IllegalArgumentException {
+
+    public NegativeSizeException() {
+    }
+
+    public NegativeSizeException(String s) {
+        super(s);
+    }
+
+    public NegativeSizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NegativeSizeException(Throwable cause) {
+        super(cause);
+    }
+}
