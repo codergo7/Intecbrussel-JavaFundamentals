376bcc70dbc03d514137733b5665d3e60e2fa33e
codergo7 <codergo7@gmail.com>
2020-12-03 21:48:54 +0100

    added generic Duo class

---
+++ Chapter_17_Generics/src/be/intecbrussel/ex_1/Duo.java
@@ -0,0 +1,28 @@
+package be.intecbrussel.ex_1;
+
+public class Duo<E> {
+
+    private E first;
+    private E second;
+
+    public Duo(E first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public void setFirst(E first) {
+        this.first = first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public void setSecond(E second) {
+        this.second = second;
+    }
+}
