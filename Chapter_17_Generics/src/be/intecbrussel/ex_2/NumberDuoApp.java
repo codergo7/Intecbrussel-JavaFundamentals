fe96f4ac565dd7c57e637b8c2a8c0a6f0297d581
codergo7 <codergo7@gmail.com>
2020-12-04 09:57:26 +0100

    edited

--- Chapter_17_Generics/src/be/intecbrussel/ex_2/NumberDuoApp.java
+++ Chapter_17_Generics/src/be/intecbrussel/ex_2/NumberDuoApp.java
@@ -1,12 +1,23 @@
 package be.intecbrussel.ex_2;
 
+import java.io.Serializable;
+
 public class NumberDuoApp {
 
     public static void main(String[] args) {
 
         NumberDuo<Integer> numberDuo = new NumberDuo<Integer>(5,7);
-
         System.out.println(numberDuo.getSum());
+
+        NumberDuo<?> numberDuo2 = new NumberDuo<Integer>(5,7);
+
+        //Integer i = numberDuo2.getFirst(); // can not be compiled
+
+        Number number = numberDuo2.getFirst();
+
+        System.out.println(number);
+
+
     }
 
 
