7f62e38ecda313b422a2d3cde396f1840fd8b969
codergo7 <codergo7@gmail.com>
2020-12-04 09:57:06 +0100

    added subclasses IntegerDuo and StringDuo

---
+++ Chapter_17_Generics/src/be/intecbrussel/ex_3/IntegerDuo.java
@@ -0,0 +1,10 @@
+package be.intecbrussel.ex_3;
+
+import be.intecbrussel.ex_1.Duo;
+
+public class IntegerDuo extends Duo<Integer> {
+
+    public IntegerDuo(Integer first, Integer second) {
+        super(first, second);
+    }
+}
---
+++ Chapter_17_Generics/src/be/intecbrussel/ex_3/StringDuo.java
@@ -0,0 +1,9 @@
+package be.intecbrussel.ex_3;
+
+import be.intecbrussel.ex_1.Duo;
+
+public class StringDuo extends Duo<String> {
+    public StringDuo(String first, String second) {
+        super(first, second);
+    }
+}
