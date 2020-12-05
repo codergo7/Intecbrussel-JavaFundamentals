9d8374d53eff967f262d5b6bc1f6316501f8a505
codergo7 <codergo7@gmail.com>
2020-12-05 12:50:34 +0100

    added and completed NotEnoughMoneyException

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/gameshop/NotEnoughMoneyException.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.gameshop;
+
+public class NotEnoughMoneyException extends Exception{
+
+    public NotEnoughMoneyException() {
+    }
+
+    public NotEnoughMoneyException(String message) {
+        super(message);
+    }
+
+    public NotEnoughMoneyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughMoneyException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotEnoughMoneyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
