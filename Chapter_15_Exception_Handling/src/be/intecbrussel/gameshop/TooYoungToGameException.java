057331879db19d5068f19e6bb6dc8011110422f2
codergo7 <codergo7@gmail.com>
2020-12-05 12:48:58 +0100

    added and completed TooYoungToGameException

---
+++ Chapter_15_Exception_Handling/src/be/intecbrussel/gameshop/TooYoungToGameException.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.gameshop;
+
+public class TooYoungToGameException extends Exception{
+
+    public TooYoungToGameException() {
+    }
+
+    public TooYoungToGameException(String message) {
+        super(message);
+    }
+
+    public TooYoungToGameException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TooYoungToGameException(Throwable cause) {
+        super(cause);
+    }
+
+    public TooYoungToGameException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
