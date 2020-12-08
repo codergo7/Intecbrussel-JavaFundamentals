8c2df649af9faca575c87141f9e24b3d28a3abba
codergo7 <codergo7@gmail.com>
2020-12-08 13:23:16 +0100

    completed

--- Chapter_18_Lambda_Expressions/src/be/intecbrussel/ex_1/TextApp.java
+++ Chapter_18_Lambda_Expressions/src/be/intecbrussel/ex_1/TextApp.java
@@ -19,11 +19,43 @@ public class TextApp {
         // with lambda expressions
         tp.printFilteredWords(s -> s.contains("e"));
 
-        System.out.println("\n*** Words which are greater than 4 in length ***");
+        System.out.println("\n*** Long words ***");
         tp.printFilteredWords(s->s.length()>4);
 
         System.out.println("\n*** Words starting with with 'a' ***");
         tp.printFilteredWords(s -> s.startsWith("a"));
+
+        System.out.println("\n*** Words which second letter is 'e' ***");
+        tp.printFilteredWords(word -> word.length()>1 && word.substring(1,2).equals("e"));
+
+        System.out.println("\n*** Words containing more than one 'e' ***");
+        tp.printFilteredWords(word -> word.indexOf('e')!=word.lastIndexOf('e'));
+
+        System.out.println("\n*** Words containing two 'e' with anonymous nested class ***");
+        tp.printFilteredWords(new WordFilter() {
+            @Override
+            public boolean isValid(String s) {
+                int amountOfE= 0;
+                for(String s1 :s.split("")){
+                    if(s1.equals("e")){
+                        amountOfE++;
+                    }
+                }
+                return amountOfE==2;
+            }
+        });
+
+        System.out.println("\n*** Words containing two 'e' with lambda ***");
+        tp.printFilteredWords(word ->{
+            int amountOfE= 0;
+            for(String s1 :word.split("")){
+                if(s1.equals("e")){
+                    amountOfE++;
+                }
+            }
+            return amountOfE==2;
+        });
+
 
     }
 }
