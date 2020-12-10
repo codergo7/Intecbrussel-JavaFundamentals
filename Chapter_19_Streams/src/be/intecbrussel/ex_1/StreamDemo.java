c2e4996b573f72ea60aaa4065a50c165e7566e8b
codergo7 <codergo7@gmail.com>
2020-12-10 09:33:40 +0100

    added StreamDemo class

---
+++ Chapter_19_Streams/src/be/intecbrussel/ex_1/StreamDemo.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.ex_1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+
+    public static void main(String[] args) {
+        String[] strings = {"Hello", "class","of", "Java","EE", "2020"};
+
+        // print string array
+        //Arrays.stream(strings).forEach(str-> System.out.printf("%s ", str));
+        //Stream.of(strings).forEach(System.out::println);
+        Stream.of(strings).forEach(s -> System.out.printf("%s ",s));
+        System.out.println();
+
+        // print 0-99
+        IntStream.rangeClosed(1,100).forEach(i-> System.out.printf("%d ",i));
+
+
+    }
+}
