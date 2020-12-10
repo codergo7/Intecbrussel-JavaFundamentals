fc7dd95314c988315469fe0841c327594739b429
codergo7 <codergo7@gmail.com>
2020-12-10 09:40:07 +0100

    added Person class

---
+++ Chapter_19_Streams/src/be/intecbrussel/ex_1/Person.java
@@ -0,0 +1,55 @@
+package be.intecbrussel.ex_1;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private String gender;
+    private int age;
+    private float weight;
+    private float height;
+
+    public Person(String name, String surname, String gender, int age, float weight, float height) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: " +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", height=" + height;
+    }
+}
