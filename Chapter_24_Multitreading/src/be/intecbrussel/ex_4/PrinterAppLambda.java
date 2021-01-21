85d873af93b7bafb89ff2199d05eb48f5fa99c06
codergo7 <codergo7@gmail.com>
2021-01-21 19:04:41 +0100

    added ex-4

---
+++ Chapter_24_Multitreading/src/be/intecbrussel/ex_4/PrinterAppLambda.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.ex_4;
+
+public class PrinterAppLambda {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(()-> print('1',10));
+        Thread t2 = new Thread(()-> print('2',1000));
+
+        t2.setDaemon(true);
+        t1.setDaemon(true);
+        t1.start();
+        t2.start();
+
+    }
+
+    private static void print(char c, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print(c + " ");
+            Thread.yield();
+        }
+    }
+
+
+}
