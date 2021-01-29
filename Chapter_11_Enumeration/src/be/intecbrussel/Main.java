2b3d7d471d96360923d7c5a77a4d6365d0f1c6b6
codergo7 <codergo7@gmail.com>
2021-01-29 23:45:49 +0100

    clean code

--- Chapter_11_Enumeration/src/be/intecbrussel/Main.java
+++ Chapter_11_Enumeration/src/be/intecbrussel/Main.java
@@ -3,6 +3,6 @@ package be.intecbrussel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello");
     }
 }
--- FoodShop/src/be/intecbrussel/service/CustomerRepository.java
+++
@@ -1,4 +0,0 @@
-package be.intecbrussel.foodshop.service;
-
-public class CustomerRepository {
-}
